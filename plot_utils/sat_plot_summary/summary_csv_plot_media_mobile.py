d6b2647c97b313b5740049b7480abbc40cb3f2d0
mm918817 <m.maggioni57@campus.unimib.it>
2026-05-15 18:56:35 +0200

    Translated the prints and text to english

--- plot_utils/sat_plot_summary/summary_csv_plot_media_mobile.py
+++ plot_utils/sat_plot_summary/summary_csv_plot_media_mobile.py
@@ -9,10 +9,10 @@ output_folder = "plots"        # Dove salva i grafici
 os.makedirs(output_folder, exist_ok=True)
 
 X_SCALE = 5 # Evaluation ogni 5 iterazioni, nel summary.csv sono salvate ogni 1
-WINDOW_SIZE = 10  # Media mobile su 10 punti (50 iterazioni reali se X_SCALE=5)
-WINDOW_SIZE_OPT = 10 # Media mobile per rotta ottimale e con errori per poter calcolare: media, min e max su questo range di iterazioni
-WINDOW_SIZE_OPT_FINAL = 10 # Media mobile per tabella riassuntiva rotta ottimale
-WINDOW_SIZE_REWARD = 10 # Media mobile per il reward
+WINDOW_SIZE = 50  # Media mobile su 10 punti (50 iterazioni reali se X_SCALE=5)
+WINDOW_SIZE_OPT = 50 # Media mobile per rotta ottimale e con errori per poter calcolare: media, min e max su questo range di iterazioni
+WINDOW_SIZE_OPT_FINAL = 50 # Media mobile per tabella riassuntiva rotta ottimale
+WINDOW_SIZE_REWARD = 50 # Media mobile per il reward
 # Parametri per la penalità (Per grafico scostamento rispetto a dijktra) 
 MAX_PENALTY = 700 # Valore assegnato ad ogni flusso NON concluso (eg: 350%, usare il massimo tra i vari summary, mettere a 0 con anche WINDOW_SIZE ad 1 e vedere il valore massimo che esce)
 TOT_FLOWS = 10 # Numero di flussi per ogni valutazione
@@ -20,17 +20,27 @@ TOT_FLOWS = 10 # Numero di flussi per ogni valutazione
 # File summary.csv da includere
 files_to_plot = [
 
+    "1",
+    "2"
+    #"A1 imit bonus 0.05",
+    #"B1 imit bonus 0.05 lr 3k",
+    #"B2 imit bonus 0.05 lr 3k",
+    #"C1 imit bonus 0.05 lr 3k, eps 0.4",
+    #"C2 imit bonus 0.05 lr 3k, eps 0.4",+
+    #"D1 imit bonus 0.05 lr 3k, eps 0.4, 2k reward",
+    #"D2 imit bonus 0.05 lr 3k, eps 0.4, 2k reward",
 
+    #"bonus 3k 0.4 2k imit 22500",
 
-    "main cur E best imit",
-    "test cur E1 750",
-    "test cur E1 1500",
-    #"test cur",
-    #"main cur",
+    #"E1",
+    #"E2",
+    #"F1",
+    #F2",
 
+    #"E1 - n_step 5",
+    #"E2 - n_step 5"
     #"E1 - n_step 5, 2k iter, 120k eps",
     
-
     #"cur E2 reset eps 120k e lr 0.0002",
     #"cur E3 reset eps 120k e lr 0.0002",
     #"cur E1 reset eps 120k e lr 0.0002, 4000 it",
@@ -42,10 +52,42 @@ files_to_plot = [
 # Stile linee (se non specificato usa il default di Matplotlib)
 file_styles = {
 
-    "envmain E": {"color": "black", "linestyle": "--", "linewidth": 1},
-    #"cur E1 reset eps 120k e lr 0.0002, 4000 it": {"color": "tab:orange", "linestyle": "-", "linewidth": 1},
-    #"cur E2 reset eps 120k e lr 0.0002, 4000 it": {"color": "tab:blue", "linestyle": "-", "linewidth": 1},
-    #"E1": {"color": "tab:blue", "linestyle": "-", "linewidth": 1},
+    "A1 imit bonus 0.05": {"linestyle": "-", "linewidth": 0.5},
+    "B1 imit bonus 0.05 lr 3k": {"linestyle": "-", "linewidth": 0.5},
+    "B2 imit bonus 0.05 lr 3k": {"linestyle": "-", "linewidth": 0.5},
+    "C1 imit bonus 0.05 lr 3k, eps 0.4": {"linestyle": "-", "linewidth": 0.5},
+    "C2 imit bonus 0.05 lr 3k, eps 0.4": {"linestyle": "-", "linewidth": 0.5},
+    "D1 imit bonus 0.05 lr 3k, eps 0.4, 2k reward": {"linestyle": "-", "linewidth": 0.5},
+    "D2 imit bonus 0.05 lr 3k, eps 0.4, 2k reward": {"linestyle": "-", "linewidth": 0.5},
+
+    "bonus 3k 0.4 2k imit 22500": {"linestyle": "-", "linewidth": 0.5},
+
+    "E1": {"linestyle": "-", "linewidth": 0.5},
+    "E2": {"linestyle": "-", "linewidth": 0.5},
+    "F1": {"linestyle": "-", "linewidth": 0.5},
+    "F2": {"linestyle": "-", "linewidth": 0.5},
+    
+    "bonus 3k 0.4 2k imit 22500": {"linestyle": "-", "linewidth": 0.5},
+
+
+    #"A1 - dest 1, step dijk30": {"color": "tab:orange", "linestyle": "--","alpha": 0.5, "linewidth": 1},
+    #"A2 - dest 1, step dijk30": {"color": "tab:orange", "linestyle": "-", "linewidth": 1},
+    "B2 - dest din, step dijk30": {"color": "tab:grey", "linestyle": ":","alpha": 0.5, "linewidth": 1},
+    "A1 - n_step 1": {"color": "tab:grey", "linestyle": ":","alpha": 0.5, "linewidth": 1},
+    "A3 - n_step 1": {"color": "tab:grey", "linestyle": ":","alpha": 0.5, "linewidth": 1},
+    "A2 - n_step 1": {"color": "tab:blue", "linestyle": "-", "linewidth": 1},
+
+
+    "cur E1 reset eps 120k e lr 0.0002, 4000 it": {"color": "tab:red", "linestyle": ":","alpha": 0.5, "linewidth": 1},
+    "cur E2 reset eps 120k e lr 0.0002, 4000 it": {"color": "tab:purple", "linestyle": "-", "linewidth": 1},
+    #"A2 - dest 1, step dijk30": {"color": "tab:blue", "linestyle": "-", "linewidth": 1},
+    #"B3 - dest din, step dijk30": {"color": "tab:orange", "linestyle": "-", "linewidth": 1},
+    #"A3 - dest 1, step dijk30": {"color": "tab:grey", "linestyle": ":","alpha": 0.5, "linewidth": 1},
+    
+
+
+
+    #"E1": {"color": "tab:blue", "linestyle": "-", "alpha": 0.3, "linewidth": 1},
     #"E2": {"color": "tab:orange", "linestyle": "-", "linewidth": 1},
     #"E3": {"color": "tab:green", "linestyle": "-", "linewidth": 1},
 
@@ -54,20 +96,20 @@ file_styles = {
 
 # Metriche ed i titoli (grafici che stampa)
 metrics = [
-    ("pct_concluded", "Percentuale Flussi Conclusi (%)", "concluded_plot.png"),
-    ("pct_optimal", "Percentuale Rotte Ottimali (%)", "err0_optimal_plot.png"),
-    ("pct_err10", "Percentuale Rotte Ottimali, entro errore 10% (%)", "err10_plot.png"),
-    ("pct_err20", "Percentuale Rotte Ottimali, entro errore 20% (%)", "err20_plot.png"),
-    ("pct_err30", "Percentuale Rotte Ottimali, entro errore 30% (%)", "err30_plot.png"),
-    ("pct_err40", "Percentuale Rotte Ottimali, entro errore 40% (%)", "err40_plot.png"),
-    ("pct_err50", "Percentuale Rotte Ottimali, entro errore 50% (%)", "err50_plot.png"),
-    ("mean_diff_iter", "Scostamento Medio da Dijkstra (%)", "deviation_plot.png"),
-    ("mean_reward_iter", "Reward Medio per Iterazione (con min e max)", "reward_plot.png")
+    ("pct_concluded", "Completed Flows Percentage (%)", "concluded_plot.png"),
+    ("pct_optimal", "Optimal Routes Percentage (%)", "err0_optimal_plot.png"),
+    ("pct_err10", "Optimal Routes Percentage, within 10% error (%)", "err10_plot.png"),
+    ("pct_err20", "Optimal Routes Percentage, within 20% error (%)", "err20_plot.png"),
+    ("pct_err30", "Optimal Routes Percentage, within 30% error (%)", "err30_plot.png"),
+    ("pct_err40", "Optimal Routes Percentage, within 40% error (%)", "err40_plot.png"),
+    ("pct_err50", "Optimal Routes Percentage, within 50% error (%)", "err50_plot.png"),
+    ("mean_diff_iter", "Average Deviation from Dijkstra (%)", "deviation_plot.png"),
+    ("mean_reward_iter", "Average Reward per Iteration (min, max)", "reward_plot.png")
 ]
 
 # --- 1. GENERAZIONE PLOT % METRICHE PER ITERAZIONI---
 if not files_to_plot:
-    print("Nessun file specificato.")
+    print("No file specified.")
 else:
     for column, ylabel, filename in metrics:
         plt.figure(figsize=(12, 6))
@@ -113,7 +155,7 @@ else:
                     # Trasformandole in argomenti per la funzione (usato per gli stili)
                     line, = plt.plot(x_values, y_smooth, label=file_name, **custom_args)
                     plt.fill_between(x_values, y_min_window, y_max_window, color=line.get_color(), alpha=0.1)
-                    plt.title(f"Confronto (Media Mobile {WINDOW_SIZE_OPT} punti): {ylabel}")
+                    plt.title(f"Comparison ({WINDOW_SIZE_OPT}-points Moving Average): {ylabel}")
 
                 # --- CASE 2: Scostamento da Dijkstra (Media, Min/Max e Std Dev rispetto a un'iterazione, cioè riga del csv)
                 # Poi viene comunque fatta una meida rispetto alla finestra specificata per rendere più leggibile
@@ -146,7 +188,7 @@ else:
                         # Per area tra i minimi e i massimi
                         plt.fill_between(x_values, y_min_smooth.clip(lower=0), y_max_smooth, 
                                         color=line.get_color(), alpha=0.05)
-                        plt.title(f"Confronto (Media Mobile {WINDOW_SIZE} punti): {ylabel}")
+                        plt.title(f"Comparison ({WINDOW_SIZE}-points Moving Average): {ylabel}")
 
                 # --- CASE 4: Reward dell'iterazione (Media con ombra Min/Max, di cui si applica una finestra specificata ) ---
                 elif column == "mean_reward_iter":
@@ -160,21 +202,22 @@ else:
                     plt.fill_between(x_values, y_min_smooth, y_max_smooth, 
                                      color=line.get_color(), alpha=0.15)
                     
-                    plt.title(f"Andamento Reward (Media Mobile {WINDOW_SIZE_REWARD} punti): {ylabel}")
+                    plt.title(f"Reward Trend ({WINDOW_SIZE_REWARD}-points Moving Average): {ylabel}")
                 # --- CASE 3: Flussi conclusi (Media rispetto alla finestra specificata)
                 else:
                     y_smooth = df[column].rolling(window=WINDOW_SIZE, min_periods=1).mean()
                     plt.plot(x_values, y_smooth, label=file_name, **custom_args)
-                    plt.title(f"Confronto (Media Mobile {WINDOW_SIZE} punti): {ylabel}")
+                    plt.title(f"Comparison ({WINDOW_SIZE}-points Moving Average): {ylabel}")
 
         
         if column == "mean_diff_iter":
-            plt.axhline(y=25, color='gold', linestyle='--', linewidth=1, label='Soglie 25 e 50%')
-            plt.axhline(y=50, color='gold', linestyle='--', linewidth=1)
-            plt.axhline(y=10, color='black', linestyle='--', linewidth=1, label='Soglia 10%')
+            plt.axhline(y=25, color='gold', linestyle='--', linewidth=0.5, label='Thresholds 25, 50%')
+            plt.axhline(y=50, color='gold', linestyle='--', linewidth=0.5)
+            plt.axhline(y=10, color='black', linestyle='--', linewidth=0.5, label='Threshold 10%')
+            plt.axhline(y=5, color='purple', linestyle='--', linewidth=0.5, label='Threshold 5%')
 
 
-        plt.xlabel("Iterazioni")
+        plt.xlabel("Iterations")
         plt.ylabel(ylabel)
         plt.grid(True, linestyle='--', alpha=0.6)
 
@@ -187,7 +230,7 @@ else:
         # Salva il grafico
         plt.savefig(os.path.join(output_folder, filename), dpi=300)
         plt.close()
-        print(f"Grafico generato: {filename}")
+        print(f"Generated graph: {filename}")
 
     # --- 2. GENERAZIONE GRAFICO MAX PERFORMANCE PER ERRORE ---
     plt.figure(figsize=(10, 6))
@@ -206,9 +249,9 @@ else:
             custom_args = file_styles.get(file_name, {})
             plt.plot(error_thresholds, max_values, marker='o', label=file_name, **custom_args) 
 
-    plt.title("Massima Accuratezza raggiunta per Soglia di Errore")
-    plt.xlabel("Soglia di Errore Tollerata (%)")
-    plt.ylabel("Massima Percentuale Raggiunta (%)")
+    plt.title("Maximum Accuracy achieved for Error Threshold")
+    plt.xlabel("Tolerated Error Threshold (%)")
+    plt.ylabel("Maximum Percentage Reached (%)")
     plt.ylim(0, 105)
     plt.xticks(error_thresholds) # Forza i valori 0, 10, 20... sull'asse x
     plt.grid(True, linestyle='--', alpha=0.6)
@@ -217,10 +260,10 @@ else:
     summary_filename = "max_performance_error_summary.png"
     plt.savefig(os.path.join(output_folder, summary_filename), dpi=300)
     plt.close()
-    print(f"Grafico riassuntivo errore, generato: {summary_filename}")
+    print(f"Error summary graph, generated: {summary_filename}")
 
 # --- 3. GENERAZIONE TABELLA VALORE FINALE PER ROTTE OTTIMALI CONSIDERANDO ERRORE ---
-print("\nGenerazione tabella riassuntiva valore finale per Rotte ottimali...")
+print("\nGenerate final value summary table for optimal routes...")
 
 final_results = []
 error_columns = ["pct_optimal", "pct_err10", "pct_err20", "pct_err30", "pct_err40", "pct_err50"]
@@ -232,7 +275,7 @@ for file_name in files_to_plot:
         df = pd.read_csv(file_path)
         
         # Dizionario per contenere i dati della riga corrente, dove la prima colonna è il nome della configurazione
-        row_data = {"Configurazione": file_name}
+        row_data = {"Configuration": file_name}
         
         for col in error_columns:
             if col in df.columns:
@@ -257,12 +300,12 @@ df_final_table = pd.DataFrame(final_results)
 
 # Rinomina le colonne per renderle più leggibili nella tabella
 rename_dict = {
-    "pct_optimal": "Ottimale (0%)",
-    "pct_err10": "Errore ≤10%",
-    "pct_err20": "Errore ≤20%",
-    "pct_err30": "Errore ≤30%",
-    "pct_err40": "Errore ≤40%",
-    "pct_err50": "Errore ≤50%"
+    "pct_optimal": "Optimal (0%)",
+    "pct_err10": "Error ≤10%",
+    "pct_err20": "Error ≤20%",
+    "pct_err30": "Error ≤30%",
+    "pct_err40": "Error ≤40%",
+    "pct_err50": "Error ≤50%"
 }
 df_final_table = df_final_table.rename(columns=rename_dict)
 
@@ -271,9 +314,9 @@ table_filename = os.path.join(output_folder, "final_performance_table.csv")
 df_final_table.to_csv(table_filename, index=False)
 
 # Stampa a terminale la tabella per debug
-print("\n--- PERFORMANCE ALL'ULTIMA ITERAZIONE (Media Mobile) ---")
+print("\n--- LAST ITERATION PERFORMANCE (Moving Average) ---")
 print(df_final_table.to_string(index=False))
-print(f"\nTabella salvata in: {table_filename}")
+print(f"\nTable saved at: {table_filename}")
 
-print("\nProcesso completato!")
+print("\nProcess Completed!")
 time.sleep(5)
